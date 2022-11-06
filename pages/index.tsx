79f35747d664aaaa672d68a81ac7767d730d0d5c
NΞ◎N <90662438+0xNeon-opensource@users.noreply.github.com>
2022-11-06 15:38:34 -0600

    Upgrade package

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,47 @@
 import type { NextPage } from 'next'
-import { EthosWrapper, SignInButton, ethos } from 'ethos-wallet-beta';
-import { useEffect, useState } from 'react';
+import { EthosWrapper, SignInButton, ethos } from 'ethos-connect';
+import { useCallback, useEffect, useState } from 'react';
 import { RefreshIcon } from '@heroicons/react/solid';
+import ExampleIcon from '../icons/ExampleIcon';
 
 const Home: NextPage = () => {
+  const contractAddress = '0x0000000000000000000000000000000000000002';
   const [signer, setSigner] = useState<any>(undefined);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [address, setAddress] = useState('');
   const [walletBalance, setWalletBalance] = useState('');
 
+  const { connected, connecting, getAddress } = ethos.useSuiWalletConnect()
+  const { contents } = ethos.useContents();
+
+
   const ethosConfiguration = {
     // When testing, use our staging link. When in production you may comment this line out.
-    walletAppUrl: 'https://sui-wallet-staging.onrender.com',
-    appId: 'ethos-example',
+    // walletAppUrl: 'https://sui-wallet-staging.onrender.com',
+    appId: 'ethos-example-app',
   }
 
   const onWalletConnected = async (provider: any, signer: any) => {
-    setSigner(signer);
-    const address = await signer?.getAddress();
-    setAddress(address);
-    refreshWalletBalance(address)
+    if (signer) {
+      setSigner(signer);
+      const address = await getAddress();
+      setAddress(address);
+      refreshWalletBalance(address)
+      console.log('contents :>> ', contents);
+      const walletContents = await ethos.getWalletContents(address);
+      console.log('ethos.getWalletContents() :>> ', walletContents);
+    }
   }
 
   const onLogout = () => {
-    ethos.logout(true);
+    ethos.logout(signer);
     setSigner(undefined);
   }
 
   const refreshWalletBalance = async (address: string) => {
     setWalletBalance('...');
-    const balance = await ethos.getWalletBalance(address);
-    setWalletBalance(balance);
+    const walletContents = await ethos.getWalletContents(address);
+    setWalletBalance(walletContents.suiBalance.toString());
   }
 
   const fund = async () => {
@@ -41,52 +52,43 @@ const Home: NextPage = () => {
 
   const mint = async () => {
     try {
-      const details = {
-        network: 'sui',
-        chain: 'sui',
-        walletAddress: address,
-        address: '0x0000000000000000000000000000000000000002',
-        moduleName: 'devnet_nft',
-        functionName: 'mint',
-        inputValues: [
-          "Example NFT Name",
-          'This is a description',
-          'https://ethoswallet.xyz/assets/images/ethos-email-logo.png'
-        ],
-        gasBudget: 1000
-      }
-      const _address = await signer.getAddress()
-      console.log('_address :>> ', _address);
+      const signableTransaction = {
+        kind: "moveCall" as const,
+        data: {
+          packageObjectId: contractAddress,
+          module: 'devnet_nft',
+          function: 'mint',
+          typeArguments: [],
+          arguments: [
+            "Example NFT Name",
+            'This is a description',
+            'https://ethoswallet.xyz/assets/images/ethos-email-logo.png'
+          ],
+          gasBudget: 10000
+        }
+      };
 
       ethos.transact({
         signer,
-        details,
-        onSigned: () => console.log('tx signed'),
-        onCanceled: () => console.log('tx cancelled'),
-        onSent: () => console.log('tx sent'),
-        onCompleted: async (result: any) => {
-          await result;
-          console.log("COMPLETED RESULT", result)
-          ethos.hideWallet();
-        }
+        signableTransaction,
       })
     } catch (error) {
       console.log(error);
     }
   }
 
-  useEffect(() => {
-    setLoading(false)
-  }, []);
-
-  if (loading) {
-    return <div>Loading...</div>
-  }
-
   return (
     <EthosWrapper
       ethosConfiguration={ethosConfiguration}
-      onWalletConnected={({ provider, signer }) => onWalletConnected(provider, signer)}>
+      onWalletConnected={({ provider, signer }) => onWalletConnected(provider, signer)}
+      dappName='EthosConnect Example App'
+      dappIcon={<ExampleIcon />}
+      connectMessage='Your connect message goes here!'
+    >
+      {/* Connected: {connected ? 'true' : 'false'}
+      <br />
+      Connecting: {connecting ? 'true' : 'false'} */}
+
       <div className="max-w-7xl mx-auto text-center py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
         {
           !signer ? (
@@ -96,57 +98,45 @@ const Home: NextPage = () => {
               Connect
             </SignInButton>
           ) : (
-            <>
-              <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-                Connected to wallet
-              </h2>
-              <p><code>{address}</code></p>
-              <p>
-                <span className="inline-flex items-center text-base font-medium text-ethos-primary space-x-1">
+            <div className='flex flex-col gap-6'>
+              <div className='flex flex-col gap-2'>
+                <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                  Connected to wallet
+                </h2>
+                <code>{address}</code>
+                <div className="flex flex-row place-content-center text-base font-medium text-ethos-primary space-x-1">
                   <span>Wallet balance: <code>{walletBalance}</code></span>
                   <RefreshIcon
                     className="h-5 w-5  text-blue-500 cursor-pointer"
                     aria-hidden="true"
                     onClick={() => refreshWalletBalance(address)}
                   />
-                </span>
-              </p>
-              <p className='py-4'>
+                </div>
+              </div>
+              <div className='flex flex-col gap-4'>
                 First, fund this wallet from the Sui faucet:
-              </p>
-              <button
-                className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
-                onClick={fund}
-              >
-                Fund
-              </button>
-              <p className='py-4'>
-                then
-              </p>
-              <button
-                className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
-                onClick={mint}
-              >
-                Mint an NFT
-              </button>
-              <p className='py-4'>
-                or
-              </p>
-              <div>
                 <button
-                  className="mx-2 inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
-                  onClick={ethos.showWallet}
+                  className="mx-auto px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
+                  onClick={fund}
                 >
-                  Show wallet
+                  Fund
                 </button>
+                then
                 <button
-                  className="mx-2 inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
+                  className="mx-auto px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
+                  onClick={mint}
+                >
+                  Mint an NFT
+                </button>
+                or
+                <button
+                  className="mx-auto px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
                   onClick={onLogout}
                 >
                   Sign Out
                 </button>
               </div>
-            </>
+            </div>
           )
         }
       </div>
