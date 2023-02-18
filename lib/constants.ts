aee5f75904ef5f1c41651f4865feb863f15a91e9
Jared Cosulich <jared.cosulich@gmail.com>
2023-02-17 20:59:11 -0500

    new contract

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 export const NETWORK = 'https://fullnode.devnet.sui.io/'
-export const ETHOS_EXAMPLE_CONTRACT = "0xd19a3fd04c5c3ffe79922b06ed971e9da1ace0aa"
-export const ETHOS_EXAMPLE_COIN_TREASURY_CAP = "0x91eb4b4cebd8e9a417e12c425b41892eff666738"
-export const ETHOS_COIN_TYPE = "0xd19a3fd04c5c3ffe79922b06ed971e9da1ace0aa::ethos_example_coin::ETHOS_EXAMPLE_COIN"
+export const ETHOS_EXAMPLE_CONTRACT = "0x3309ded830ca1aa7558547e1d341d92efffaec7d"
+export const ETHOS_EXAMPLE_COIN_TREASURY_CAP = "0x7269396b1f4150abfc48bd2674137b68d29c0833"
+export const ETHOS_COIN_TYPE = "0x3309ded830ca1aa7558547e1d341d92efffaec7d::ethos_example_coin::ETHOS_EXAMPLE_COIN"
