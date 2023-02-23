a9bdc449d0c2e93de572cc1a57dca2f932833733
Adam Wade <adamwade2384@gmail.com>
2023-02-23 13:03:20 -0800

    Fix network constant to match ethos-connect

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
-export const NETWORK = 'https://fullnode.devnet.sui.io/'
+export const NETWORK = "DEVNET"
 export const ETHOS_EXAMPLE_CONTRACT = "0x0fa819f6a358ecbd5f3d25d9b8cb83c27c872761"
 export const ETHOS_EXAMPLE_COIN_TREASURY_CAP = "0x21474b84173f20ceca075129b4e6608a7831bcb0"
 export const ETHOS_COIN_TYPE = "0xfa819f6a358ecbd5f3d25d9b8cb83c27c872761::ethos_example_coin::ETHOS_EXAMPLE_COIN"
