ad366de83fc4c3d2f14b1ea8008bfe99bd4bca9b
Jared Cosulich <jared.cosulich@gmail.com>
2023-04-18 06:27:50 -0400

    new devnet contract

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,5 @@
 export const NETWORK = process.env.NETWORK || process.env.NEXT_PUBLIC_NETWORK
 export const FAUCET = process.env.FAUCET || process.env.NEXT_PUBLIC_FAUCET
-export const ETHOS_EXAMPLE_CONTRACT = "0xb2a4c86233ad80cfa5706a54fabf87d589f2f34e5d8d5d4967a0c143beb17cf6"
-export const ETHOS_EXAMPLE_COIN_TREASURY_CAP = "0xad12dfc376a1cb0e09f33bd5f6e4dd53138c2e88b9dfa45341088ba5f3debbe7"
+export const ETHOS_EXAMPLE_CONTRACT = "0x9877e0cac9acceb710e1308f3b0e129fc8a2f437fe2fa0505b9dc95eeafbf0ce"
+export const ETHOS_EXAMPLE_COIN_TREASURY_CAP = "0x3c7f933947a250063299b9623ed41ba412b06577e6f150f713d87f3eef725"
 export const ETHOS_COIN_TYPE = `${ETHOS_EXAMPLE_CONTRACT}::ethos_example_coin::ETHOS_EXAMPLE_COIN`
